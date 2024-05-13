511682c745ebdf827dff36ce798236763637060a
asma.gaini@yahoo.com <asma.gaini@yahoo.com>
2024-05-13 14:37:13 +0330

    debug svg with reCreate without changecolor

--- js/magic_sound.js
+++ js/magic_sound.js
@@ -371,38 +371,50 @@ function createItemOfPlaylist(ulTag) {
     imageDiv.appendChild(imageTag);
 
     //CREATE svg play
-    const playSvg = document.createElement("svg");
-
-    playSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    playSvg.setAttribute("viewBox", "0 0 24 24");
+    const playSvg = document.createElement("img");
+    playSvg.setAttribute("src", "../image/svg/play.svg");
+    playSvg.setAttribute("class", "svg");
 
     imageDiv.appendChild(playSvg);
 
-    const pathPlaySvg = document.createElement("path");
+    // const playSvg = document.createElement("svg");
 
-    pathPlaySvg.setAttribute(
-      "d",
-      "M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z"
-    );
+    // playSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    // playSvg.setAttribute("viewBox", "0 0 24 24");
 
-    playSvg.appendChild(pathPlaySvg);
+    // imageDiv.appendChild(playSvg);
+
+    // const pathPlaySvg = document.createElement("path");
+
+    // pathPlaySvg.setAttribute(
+    //   "d",
+    //   "M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z"
+    // );
+
+    // playSvg.appendChild(pathPlaySvg);
 
     //CREATE svg pause
-    const pauseSvg = document.createElement("svg");
-
-    pauseSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    pauseSvg.setAttribute("viewBox", "0 0 24 24");
+    const pauseSvg = document.createElement("img");
+    pauseSvg.setAttribute("src", "../image/svg/pause.svg");
+    pauseSvg.setAttribute("class", "svg");
 
     imageDiv.appendChild(pauseSvg);
 
-    const pathPauseSvg = document.createElement("path");
+    // const pauseSvg = document.createElement("svg");
 
-    pathPauseSvg.setAttribute(
-      "d",
-      "M16,2a3,3,0,0,0-3,3V19a3,3,0,0,0,6,0V5A3,3,0,0,0,16,2Zm1,17a1,1,0,0,1-2,0V5a1,1,0,0,1,2,0ZM8,2A3,3,0,0,0,5,5V19a3,3,0,0,0,6,0V5A3,3,0,0,0,8,2ZM9,19a1,1,0,0,1-2,0V5A1,1,0,0,1,9,5Z"
-    );
+    // pauseSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    // pauseSvg.setAttribute("viewBox", "0 0 24 24");
 
-    pauseSvg.appendChild(pathPauseSvg);
+    // imageDiv.appendChild(pauseSvg);
+
+    // const pathPauseSvg = document.createElement("path");
+
+    // pathPauseSvg.setAttribute(
+    //   "d",
+    //   "M16,2a3,3,0,0,0-3,3V19a3,3,0,0,0,6,0V5A3,3,0,0,0,16,2Zm1,17a1,1,0,0,1-2,0V5a1,1,0,0,1,2,0ZM8,2A3,3,0,0,0,5,5V19a3,3,0,0,0,6,0V5A3,3,0,0,0,8,2ZM9,19a1,1,0,0,1-2,0V5A1,1,0,0,1,9,5Z"
+    // );
+
+    // pauseSvg.appendChild(pathPauseSvg);
 
     //CREATE Link
     const link = document.createElement("a");
