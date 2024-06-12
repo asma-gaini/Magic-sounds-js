8a384aec90cf7e656883844491578e01faef92a1
asma.gaini@yahoo.com <asma.gaini@yahoo.com>
2024-06-12 13:13:23 +0330

    setting data music and option of song with div id in html

--- js/magic_sound.js
+++ js/magic_sound.js
@@ -1,7 +1,8 @@
 //  **************************************************** DATA  ****************************************************
-
+data_music = [];
+optionSong = [];
 //  **************************************************** DATA _ MUSIC INFORMATION  ****************************************************
-const data_music = [
+const data_music_player = [
   {
     title: "roozaye abri-1",
     artist: "jahanbakhsh",
@@ -137,9 +138,173 @@ const data_music = [
 ];
 
 //  **************************************************** DATA _ PLALIST OPTION  ****************************************************
-const optionSong = [
+const optionSong_player = [
   {
     optionName: "Add to favorite",
+    optionSvgSrc: "../image/svg/heart-empty.svg",
+    optionTarget: "#",
+    number: "1",
+  },
+  {
+    optionName: " Download",
+    optionSvgSrc: "../image/svg/download-minimalistic-svgrepo-com.svg",
+    optionTarget: "#",
+    number: "2",
+  },
+  {
+    optionName: " Share",
+    optionSvgSrc: "../image/svg/share-svgrepo-com.svg",
+    optionTarget: "#",
+    number: "3",
+  },
+  {
+    optionName: "More details",
+    optionSvgSrc: "../image/svg/details-symbolic-svgrepo-com.svg",
+    optionTarget: "#",
+    number: "4",
+  },
+];
+
+//  **************************************************** DATA _ FAVORIT MUSIC INFORMATION  ****************************************************
+const data_music_favorit = [
+  {
+    title: "roozaye abri-1",
+    artist: "jahanbakhsh",
+    audioSrc:
+      "https://dl.rozmusic.com/Music/1396/10/13/Babak%20Jahanbakhsh%20-%20Roozhaye%20Abri%20(128).mp3",
+    image: "../image/music cover/Babak Jahanbakhsh - RoozHaye Abri.jpg",
+    id: 1,
+    time: "04:49",
+    textMusic:
+      "roozaye abri-1/ روزای ابری/ I gave you your dreams/من رویا هات رو بهت دادم/ Cause you ment the world/چون تو دنیای من بودی/So did I deserve to be left in hurt/پس واقعا لیاقتم این بود که منو با این همه درد تنها بذاری/So you think I don’t know/فکر می کنی نمیدونم/You’re out of control/که تو غیر قابل کنترلی",
+  },
+  {
+    title: "Parvaz-2",
+    artist: "shahin najafi",
+    audioSrc:
+      "https://dl.ritmy.io/Single/Shahin_Najafi/Parvaz-128[ritmy.io].mp3",
+    image: "../image/music cover/shahin najafi-parvaz.jpg",
+    id: 2,
+    time: "03:40",
+    textMusic:
+      "parvaz-2/پرواز/ I gave you your dreams/من رویا هات رو بهت دادم/ Cause you ment the world/چون تو دنیای من بودی/So did I deserve to be left in hurt/پس واقعا لیاقتم این بود که منو با این همه درد تنها بذاری/So you think I don’t know/فکر می کنی نمیدونم/You’re out of control/که تو غیر قابل کنترلی",
+  },
+  {
+    title: "nistameton-3",
+    artist: "shaye",
+    audioSrc:
+      "https://dl.melonmusic.ir/Music/Shayea%20-%20Nistametoon%20(320).mp3",
+    image: "../image/music cover/shaye-nistameton.png",
+    id: 3,
+    time: "03:30",
+    textMusic:
+      "nistametoon-3/نیستمتون/ I gave you your dreams/من رویا هات رو بهت دادم/ Cause you ment the world/چون تو دنیای من بودی/So did I deserve to be left in hurt/پس واقعا لیاقتم این بود که منو با این همه درد تنها بذاری/So you think I don’t know/فکر می کنی نمیدونم/You’re out of control/که تو غیر قابل کنترلی",
+  },
+  {
+    title: "nafas-4",
+    artist: "salar aghili",
+    audioSrc:
+      "https://dl.rozmusic.com/Music/1402/10/21/Salar%20Aghili%20-%20Nafas%20%28128%29.mp3",
+    image: "../image/music cover/Babak Jahanbakhsh - RoozHaye Abri.jpg",
+    id: 4,
+    time: "03:30",
+    textMusic:
+      "nafas/نفس/ I gave you your dreams/من رویا هات رو بهت دادم/ Cause you ment the world/چون تو دنیای من بودی/So did I deserve to be left in hurt/پس واقعا لیاقتم این بود که منو با این همه درد تنها بذاری/So you think I don’t know/فکر می کنی نمیدونم/You’re out of control/که تو غیر قابل کنترلی",
+  },
+  {
+    title: "male mani-5",
+    artist: "moeen zed",
+    audioSrc:
+      "https://dl.rozmusic.com/Music/1401/12/16/Moein%20Z%20-%20Male%20Mani%20%28128%29.mp3",
+    image: "../image/music cover/shahin najafi-parvaz.jpg",
+    id: 5,
+    time: "04:23",
+    textMusic:
+      "mal mani/مال منی/ I gave you your dreams/من رویا هات رو بهت دادم/ Cause you ment the world/چون تو دنیای من بودی/So did I deserve to be left in hurt/پس واقعا لیاقتم این بود که منو با این همه درد تنها بذاری/So you think I don’t know/فکر می کنی نمیدونم/You’re out of control/که تو غیر قابل کنترلی",
+  },
+  {
+    title: "khial khosh-6",
+    artist: "alireza ghorbani",
+    audioSrc:
+      "https://dl.rozmusic.com/Music/1399/04/29/Alireza%20Ghorbani%20-%20Khiale%20Khosh%20%28128%29.mp3",
+    image: "../image/music cover/shaye-nistameton.png",
+    id: 6,
+    time: "03:49",
+    textMusic:
+      "khial khosh/خیال خوش/ I gave you your dreams/من رویا هات رو بهت دادم/ Cause you ment the world/چون تو دنیای من بودی/So did I deserve to be left in hurt/پس واقعا لیاقتم این بود که منو با این همه درد تنها بذاری/So you think I don’t know/فکر می کنی نمیدونم/You’re out of control/که تو غیر قابل کنترلی",
+  },
+  {
+    title: "yadam raft-7",
+    artist: "reza sadeghi",
+    audioSrc:
+      "https://dl.rozmusic.com/Music/1400/05/30/Reza%20Sadeghi%20-%20%20Yadam%20Raft%20%28128%29.mp3",
+    image: "../image/music cover/Babak Jahanbakhsh - RoozHaye Abri.jpg",
+    id: 7,
+    time: "03:24",
+    textMusic:
+      "yadam raft/یادم رفت/ I gave you your dreams/من رویا هات رو بهت دادم/ Cause you ment the world/چون تو دنیای من بودی/So did I deserve to be left in hurt/پس واقعا لیاقتم این بود که منو با این همه درد تنها بذاری/So you think I don’t know/فکر می کنی نمیدونم/You’re out of control/که تو غیر قابل کنترلی",
+  },
+  {
+    title: "kharabesh kardi-8",
+    artist: "farzad farzin",
+    audioSrc:
+      "https://dl.rozmusic.com/Music/1397/11/09/Farzad%20Farzin%20-%20Kharabesh%20Kardi%20(128).mp3",
+    image: "../image/music cover/shahin najafi-parvaz.jpg",
+    id: 8,
+    time: "04:04",
+    textMusic:
+      "kharabesh kardi/خرابش کردی/ I gave you your dreams/من رویا هات رو بهت دادم/ Cause you ment the world/چون تو دنیای من بودی/So did I deserve to be left in hurt/پس واقعا لیاقتم این بود که منو با این همه درد تنها بذاری/So you think I don’t know/فکر می کنی نمیدونم/You’re out of control/که تو غیر قابل کنترلی",
+  },
+  {
+    title: "koja rafte boodi-9",
+    artist: "hoorosh band",
+    audioSrc:
+      "https://hiblog.tv/1000310/file/?Ur=https://irsv.upmusics.com/AliBZ/Hoorosh%20Band%20-%20Koja%20Rafte%20Boodi%20(320).mp3&hst=irsv.upmusics.com&prt=https&cuid=1000310",
+    image: "../image/music cover/shaye-nistameton.png",
+    id: 9,
+    time: "03:26",
+    textMusic:
+      "koja rafte boodi/کجا رفته بودی/ I gave you your dreams/من رویا هات رو بهت دادم/ Cause you ment the world/چون تو دنیای من بودی/So did I deserve to be left in hurt/پس واقعا لیاقتم این بود که منو با این همه درد تنها بذاری/So you think I don’t know/فکر می کنی نمیدونم/You’re out of control/که تو غیر قابل کنترلی",
+  },
+  {
+    title: "var agar-10",
+    artist: "erfan tahmaseb",
+    audioSrc:
+      "https://hiblog.tv/1000310/file/?Ur=https://irsv.upmusics.com/AliBZ/Erfan%20Tahmasbi%20-%20Vay%20Agar%20(320).mp3&hst=irsv.upmusics.com&prt=https&cuid=1000310",
+    image: "../image/music cover/Babak Jahanbakhsh - RoozHaye Abri.jpg",
+    id: 10,
+    time: "02:56",
+    textMusic:
+      "vay agar/وای اگر/ I gave you your dreams/من رویا هات رو بهت دادم/ Cause you ment the world/چون تو دنیای من بودی/So did I deserve to be left in hurt/پس واقعا لیاقتم این بود که منو با این همه درد تنها بذاری/So you think I don’t know/فکر می کنی نمیدونم/You’re out of control/که تو غیر قابل کنترلی",
+  },
+  {
+    title: "mane divaneh-11",
+    artist: "reza bahram",
+    audioSrc:
+      "https://dl.rozmusic.com/Music/1402/07/05/Reza%20Bahram%20-%20Mane%20Divaneh%20%28128%29.mp3",
+    image: "../image/music cover/shahin najafi-parvaz.jpg",
+    id: 11,
+    time: "02:53",
+    textMusic:
+      "mane divaneh/منه دیوانه/ I gave you your dreams/من رویا هات رو بهت دادم/ Cause you ment the world/چون تو دنیای من بودی/So did I deserve to be left in hurt/پس واقعا لیاقتم این بود که منو با این همه درد تنها بذاری/So you think I don’t know/فکر می کنی نمیدونم/You’re out of control/که تو غیر قابل کنترلی",
+  },
+  {
+    title: "khastam-12",
+    artist: "behnam bani",
+    audioSrc:
+      "https://hiblog.tv/1000310/file/?Ur=https://irsv.upmusics.com/AliBZ/Behnam%20Bani%20-%20Khastam%20(320).mp3&hst=irsv.upmusics.com&prt=https&cuid=1000310",
+    image: "../image/music cover/shaye-nistameton.png",
+    id: 12,
+    time: "03:32",
+    textMusic:
+      "khastam/خستم/ I gave you your dreams/من رویا هات رو بهت دادم/ Cause you ment the world/چون تو دنیای من بودی/So did I deserve to be left in hurt/پس واقعا لیاقتم این بود که منو با این همه درد تنها بذاری/So you think I don’t know/فکر می کنی نمیدونم/You’re out of control/که تو غیر قابل کنترلی",
+  },
+];
+
+//  **************************************************** DATA _ FAVORIT PLALIST OPTION  ****************************************************
+const optionSong_favorit = [
+  {
+    optionName: "remove to favorite",
     optionSvgSrc: "../image/svg/heart-empty.svg",
     optionTarget: "#",
     number: "1",
@@ -202,6 +367,7 @@ function setMusicHasBeenCalled(clickedId) {
 
 window.onload = createPage();
 function createPage() {
+  setPlayerList();
   createPlayerTemplate();
 }
 
@@ -213,8 +379,7 @@ function createPlayerTemplate() {
 }
 
 function createPlayer() {
-  let getPlayerId = document.querySelector("#playerList");
-
+  let getPlayerId = document.getElementById("contentMusicPlaylist").children[0];
   let musicAppDiv = document.createElement("div");
   musicAppDiv.setAttribute("class", "music-app");
   getPlayerId.appendChild(musicAppDiv);
@@ -229,8 +394,7 @@ function createPlayer() {
 }
 
 function createPagination() {
-  let getPlayerId = document.querySelector("#playerList");
-
+  let getPlayerId = document.getElementById("contentMusicPlaylist").children[0];
   let paginationModalDiv = document.createElement("div");
   paginationModalDiv.setAttribute("class", "pagination-modal");
   getPlayerId.appendChild(paginationModalDiv);
@@ -435,7 +599,7 @@ function createOption() {
 
 //  **************************************************** CREATE ELEMENT _ CREATE MODAL  ****************************************************
 function createModal() {
-  let getModalId = document.querySelector("#playerList");
+  let getModalId = document.getElementById("contentMusicPlaylist").children[0];
 
   let modalDiv = document.createElement("div");
   modalDiv.setAttribute("class", "modal");
@@ -1162,5 +1326,21 @@ function displayNextPages() {
         .querySelector("li[id='" + activeId + "']")
         .classList.add("active");
     }
+  }
+}
+
+function setPlayerList() {
+  let playerId = document
+    .getElementById("contentMusicPlaylist")
+    .children[0].getAttribute("id");
+
+  if (playerId == "playerList") {
+    data_music = data_music_player;
+    optionSong = optionSong_player;
+    favoritHeart = "../image/svg/heart-empty.svg";
+  } else if (playerId == "favoritPlayList") {
+    data_music = data_music_favorit;
+    optionSong = optionSong_favorit;
+    favoritHeart = "../image/svg/heart-full.svg";
   }
 }
